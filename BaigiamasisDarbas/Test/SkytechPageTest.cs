e6dd39b2510ebeb9a8af6c3bb545a79f174dfcf7
Justinas Jankauskas <justinasjankauskas@yahoo.com>
2021-07-26 17:43:50 +0300

    Push

--- BaigiamasisDarbas/Test/SkytechPageTest.cs
+++ BaigiamasisDarbas/Test/SkytechPageTest.cs
@@ -7,22 +7,34 @@ using System.Threading.Tasks;
 
 namespace BaigiamasisDarbas.Test
 {
-    public class SkytechForumPageTest : BaseTest
+    public class SkytechPageTest : BaseTest
     {
         [Test]
-        public void Test1CreatePost() 
+        public void Test1SearhBarAndAddToCart()
+        {
+            _skytechCartPage.NavigateToDefaultPage().SearchBarInputTextAndSearh("Elgato");
+        }
+
+        [Test]
+        public void Test2MenuBarAndAddToCart()
+        {
+            _skytechCartPage.NavigateToDefaultPage().FindProductAndAddToCart();
+        }
+
+        [Test]
+        public void Test3CreatePost()
         {
             _skytechForumPage.NavigateToDefaultPage().LoginInForum().WritePost("Skundas", "Viskas neveikia taip kaip norėčiau, kad veiktų");
         }
 
         [Test]
-        public void Test2SpamCheck()
+        public void Test4SpamCheck()
         {
             _skytechForumPage.NavigateToDefaultPage().SpamPreventionCheck("Skundas", "Viskas neveikia taip kaip norėčiau, kad veiktų");
         }
 
         [Test]
-        public void Test3CreatePostWithPoll()
+        public void Test5CreatePostWithPoll()
         {
             //List max 10 elementu
             List<string> options = new List<string>();
@@ -32,5 +44,16 @@ namespace BaigiamasisDarbas.Test
             _skytechForumPage.NavigateToDefaultPage().WritePostWithPoll("Skundas", "Viskas neveikia taip kaip norėčiau, kad veiktų", "Kodėl?", options);
         }
 
+        [Test]
+        public void Test6Survey()
+        {
+            _skytechMainPage.NavigateToDefaultPage().SurveyApklausa(true, false, false, false, false).SurveyAnkstesneApklausa(false, true, false);
+        }
+
+        [Test]
+        public void Test7Login()
+        {
+            _skytechMainPage.NavigateToDefaultPage().LogIn();
+        }
     }
 }
